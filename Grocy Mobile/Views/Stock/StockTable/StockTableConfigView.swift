b1ab0d266ce87692458f7659bb0eefb782dcad4a
Georg Meißner <supergeorg@gmail.com>
2025-11-23 12:43:06 +0100

    Re-add stock table config view to not lose translations

---
+++ Grocy Mobile/Views/Stock/StockTable/StockTableConfigView.swift
@@ -0,0 +1,44 @@
+//
+//  StockTableConfigView.swift
+//  Grocy-SwiftUI
+//
+//  Created by Georg Meissner on 13.11.20.
+//
+
+import SwiftUI
+
+struct StockTableConfigView: View {
+    @Binding var showProduct: Bool
+    @Binding var showProductGroup: Bool
+    @Binding var showAmount: Bool
+    @Binding var showValue: Bool
+    @Binding var showNextBestBeforeDate: Bool
+    @Binding var showCaloriesPerStockQU: Bool
+    @Binding var showCalories: Bool
+
+    var body: some View {
+        Form {
+            Toggle("Product", isOn: $showProduct)
+            Toggle("Product group", isOn: $showProductGroup)
+            Toggle("Amount", isOn: $showAmount)
+            Toggle("Value", isOn: $showValue)
+            Toggle("Next due date", isOn: $showNextBestBeforeDate)
+            Toggle("\(Text("Calories")) (\(Text("Per stock quantity unit")))", isOn: $showCaloriesPerStockQU)
+            Toggle("Calories", isOn: $showCalories)
+        }
+    }
+}
+
+struct StockTableConfigView_Previews: PreviewProvider {
+    static var previews: some View {
+        StockTableConfigView(
+            showProduct: Binding.constant(true),
+            showProductGroup: Binding.constant(true),
+            showAmount: Binding.constant(true),
+            showValue: Binding.constant(true),
+            showNextBestBeforeDate: Binding.constant(true),
+            showCaloriesPerStockQU: Binding.constant(true),
+            showCalories: Binding.constant(true)
+        )
+    }
+}
