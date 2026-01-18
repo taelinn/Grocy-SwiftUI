d9b1475921c45a7c543d37dae141b7c5d14a8032
Georg Meißner <supergeorg@gmail.com>
2026-01-18 11:26:09 +0100

    RecipePos: Make SwiftData ready

--- Grocy Mobile/Model/Recipes/RecipePos.swift
+++ Grocy Mobile/Model/Recipes/RecipePos.swift
@@ -6,20 +6,23 @@
 //
 
 import Foundation
+import SwiftData
 
-class RecipePos: Codable {
-    var id: Int
+@Model
+final class RecipePos: Codable {
+    @Attribute(.unique) var id: Int
     var recipeID: Int
-    var productID: Int
+    var productID: Int?
     var amount: Double
-    var note: String?
-    var quID, onlyCheckSingleUnitInStock: Int
+    var note: String
+    var quID: Int?
+    var onlyCheckSingleUnitInStock: Bool
     var ingredientGroup: String?
-    var notCheckStockFulfillment: Int
-    var rowCreatedTimestamp: String
-    var variableAmount: Int?
+    var notCheckStockFulfillment: Bool
+    var variableAmount: String
     var priceFactor: Double
-    var roundUp: Int
+    var roundUp: Bool
+    var rowCreatedTimestamp: Date
 
     enum CodingKeys: String, CodingKey {
         case id
@@ -36,7 +39,39 @@ class RecipePos: Codable {
         case roundUp = "round_up"
     }
 
-    init(id: Int, recipeID: Int, productID: Int, amount: Double, note: String?, quID: Int, onlyCheckSingleUnitInStock: Int, ingredientGroup: String?, notCheckStockFulfillment: Int, rowCreatedTimestamp: String? = nil, variableAmount: Int?, priceFactor: Double, roundUp: Int) {
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        id = try container.decodeFlexibleInt(forKey: .id)
+        recipeID = try container.decodeFlexibleInt(forKey: .recipeID)
+        productID = try container.decodeFlexibleIntIfPresent(forKey: .productID)
+        amount = try container.decodeFlexibleDouble(forKey: .amount)
+        note = try container.decodeIfPresent(String.self, forKey: .note) ?? ""
+        quID = try container.decodeFlexibleIntIfPresent(forKey: .quID)
+        onlyCheckSingleUnitInStock = try container.decodeFlexibleBool(forKey: .onlyCheckSingleUnitInStock)
+        ingredientGroup = try container.decodeIfPresent(String.self, forKey: .ingredientGroup)
+        notCheckStockFulfillment = try container.decodeFlexibleBool(forKey: .notCheckStockFulfillment)
+        variableAmount = try container.decodeIfPresent(String.self, forKey: .variableAmount) ?? ""
+        priceFactor = try container.decodeFlexibleDouble(forKey: .priceFactor)
+        roundUp = try container.decodeFlexibleBool(forKey: .roundUp)
+        rowCreatedTimestamp = getDateFromString(try container.decode(String.self, forKey: .rowCreatedTimestamp))!
+    }
+
+    init(
+        id: Int = -1,
+        recipeID: Int = -1,
+        productID: Int = -1,
+        amount: Double = 1.0,
+        note: String = "",
+        quID: Int = -1,
+        onlyCheckSingleUnitInStock: Bool = false,
+        ingredientGroup: String? = nil,
+        notCheckStockFulfillment: Bool = false,
+        rowCreatedTimestamp: Date = Date(),
+        variableAmount: String = "",
+        priceFactor: Double = 1.0,
+        roundUp: Bool = false,
+    ) {
         self.id = id
         self.recipeID = recipeID
         self.productID = productID
@@ -49,6 +84,23 @@ class RecipePos: Codable {
         self.variableAmount = variableAmount
         self.priceFactor = priceFactor
         self.roundUp = roundUp
-        self.rowCreatedTimestamp = rowCreatedTimestamp ?? Date().iso8601withFractionalSeconds
+        self.rowCreatedTimestamp = rowCreatedTimestamp
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(recipeID, forKey: .recipeID)
+        try container.encode(productID, forKey: .productID)
+        try container.encode(amount, forKey: .amount)
+        try container.encode(note, forKey: .note)
+        try container.encode(quID, forKey: .quID)
+        try container.encode(onlyCheckSingleUnitInStock, forKey: .onlyCheckSingleUnitInStock)
+        try container.encode(ingredientGroup, forKey: .ingredientGroup)
+        try container.encode(notCheckStockFulfillment, forKey: .notCheckStockFulfillment)
+        try container.encode(rowCreatedTimestamp, forKey: .rowCreatedTimestamp)
+        try container.encode(variableAmount, forKey: .variableAmount)
+        try container.encode(priceFactor, forKey: .priceFactor)
+        try container.encode(roundUp, forKey: .roundUp)
     }
 }
